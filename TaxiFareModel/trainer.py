8999732eda5b977208a28c7e3e6cce4fd0208378
Tim Wright <timwright614@gmail.com>
2022-02-22 17:07:35 +0000

    challenge updates

---
+++ TaxiFareModel/trainer.py
@@ -0,0 +1,136 @@
+# imports
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from TaxiFareModel.data import get_data
+from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
+from TaxiFareModel.data import get_data, clean_data
+from TaxiFareModel.utils import compute_rmse
+from memoized_property import memoized_property
+import mlflow
+from mlflow.tracking import MlflowClient
+from datetime import datetime
+import joblib
+
+
+
+now = datetime.now()
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+EXPERIMENT_NAME = f'GB_LON_timwright614_{now}'
+
+class Trainer():
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.experiment_name = EXPERIMENT_NAME
+        self.pipeline = None
+        self.X = X
+        self.y = y
+
+
+    def set_pipeline(self):
+        """defines the pipeline as a class attribute"""
+
+        dist_pipe = Pipeline([
+            ('dist_trans', DistanceTransformer()),
+            ('stdscaler', StandardScaler())
+        ])
+        time_pipe = Pipeline([
+            ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+            ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+        preproc_pipe = ColumnTransformer([
+            ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+            ('time', time_pipe, ['pickup_datetime'])
+        ], remainder="drop")
+        pipe = Pipeline([
+            ('preproc', preproc_pipe),
+            ('linear_model', model)
+        ])
+        self.pipeline = pipe
+
+    def run(self):
+        """set and train the pipeline"""
+        self.set_pipeline()
+        self.pipeline.fit(self.X,self.y)
+
+    def evaluate(self, X_test, y_test):
+        """evaluates the pipeline on df_test and return the RMSE"""
+        y_pred = self.pipeline.predict(X_test)
+        rmse = compute_rmse(y_pred, y_test)
+        print(rmse)
+        return rmse
+
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self.pipeline, 'model.joblib')
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+
+if __name__ == "__main__":
+
+
+    # get data
+    df = get_data()
+
+    # clean data
+    df = clean_data(df, test=False)
+
+    # set X and y
+    y = df["fare_amount"]
+    X = df.drop("fare_amount", axis=1)
+
+    # hold out
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
+
+    models = [LinearRegression(), GradientBoostingRegressor()]
+
+    for model in models:
+        model=model
+
+        # train
+        trainer = Trainer(X_train,y_train)
+        trainer.run()
+
+        # save
+        trainer.save_model()
+
+        # evaluate
+        rsme = trainer.evaluate(X_val, y_val)
+
+        #logs
+
+        trainer.mlflow_log_param('estimator', model)
+
+        trainer.mlflow_log_metric('rsme', rsme)
+
+        experiment_id = trainer.mlflow_experiment_id
+
+    print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
